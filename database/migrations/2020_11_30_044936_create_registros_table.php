27cb6b6b7164dd59e66a8b004a88bd827f38f77e
KeenMcLovin <al221911456@gmail.com>
2020-11-29 23:01:36 -0600

    Migraciones

---
+++ database/migrations/2020_11_30_044936_create_registros_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->int('edad');
+            $table->string('genero');
+            $table->int('telefono');
+            $table->string('correo');
+            $table->string('contraseña');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registros');
+    }
+}
